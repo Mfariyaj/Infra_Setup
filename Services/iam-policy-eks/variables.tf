07f98140975f4621cb3dd88413b9cd3bfd63789c
Mfariyaj <ubuntu@ip-172-31-95-48.ec2.internal>
2025-01-23 14:31:28 +0000

    added the main tf file

--- Services/iam-policy-eks/variables.tf
+++ Services/iam-policy-eks/variables.tf
@@ -1,4 +1,9 @@
-variable "iam_role_arn" {
+variable "worker_arn" {
+  description = "IAM Role ARN for the worker nodes"
+  type        = string
+}
+
+variable "master_arn" {
   description = "IAM Role ARN for the worker nodes"
   type        = string
 }
