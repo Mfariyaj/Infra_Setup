0a9d574b2876fc38c7a03e31e94eae865d989edf
Mfariyaj <ubuntu@ip-172-31-95-48.ec2.internal>
2025-01-23 09:33:20 +0000

    removed the duplicate values

--- Services/VPC/Output.tf
+++ Services/VPC/Output.tf
@@ -15,10 +15,6 @@ output "public_route_table_id" {
   value = aws_route_table.public.id
 }
 
-output "private_route_table_id" {
-  value = aws_route_table.private.id
-}
-
 output "internet_gateway_id" {
   value = aws_internet_gateway.igw.id
 }
