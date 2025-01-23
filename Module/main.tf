4d534f6e96ed0c25308b1f30486c4e203eb83d1d
Mfariyaj <ubuntu@ip-172-31-95-48.ec2.internal>
2025-01-23 14:57:15 +0000

    added the main tf file

--- Module/main.tf
+++ Module/main.tf
@@ -42,7 +42,7 @@ module "EKS" {
   instance_name      = var.instance_name
   ami_id             = var.ami_id
   instance_type      = var.instance_type
-  subnet_id          = element[module.vpc.public_subnet_ids]
+  subnet_id          = module.vpc.public_subnet_ids
   security_group_ids = [module.security_group.security_group_id]
   master_arn         = module.output.master_arn
   worker_arn       = module.output.worker_arn
