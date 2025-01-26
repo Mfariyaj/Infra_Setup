17e66ca803b3aa9ec328dd559e96e5fdddff8bbb
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-26 20:01:28 +0530

    Update output.tf

--- Services/iam-policy-eks/output.tf
+++ Services/iam-policy-eks/output.tf
@@ -1,7 +1,7 @@
-output "vpc_id" {
-  value = aws_vpc.eks_vpc.id
+output "eks_cluster_role_arn" {
+  value = aws_iam_role.eks_cluster_role.arn
 }
 
-output "eks_role_arn" {
-  value = aws_iam_role.eks_role.arn
+output "eks_node_role_arn" {
+  value = aws_iam_role.eks_node_role.arn
 }
