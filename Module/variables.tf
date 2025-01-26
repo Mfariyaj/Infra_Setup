8e35ee334ac48bbd2b907a5e074402e516ea943a
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-26 18:31:36 +0530

    Update variables.tf

--- Module/variables.tf
+++ Module/variables.tf
@@ -118,10 +118,6 @@ variable "node_group_desired_size" {
   type        = number
   default     = 2
 }
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
 
 variable "eks_role_arn" {
   description = "ARN of the IAM role for the EKS cluster"
