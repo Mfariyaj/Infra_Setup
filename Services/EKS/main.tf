12165218a056fb645ac0f961d057b2ccf71f7599
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-26 20:02:21 +0530

    Update main.tf

--- Services/EKS/main.tf
+++ Services/EKS/main.tf
@@ -1,31 +1,21 @@
-resource "aws_eks_cluster" "main" {
-  name     = var.cluster_name
-  role_arn = var.eks_role_arn
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
 
-  vpc_config {
-    subnet_ids = var.private_subnet_ids
-    endpoint_public_access = true
-    endpoint_private_access = true
+  vpc_id             = var.vpc_id
+  subnet_ids         = concat(var.public_subnet_ids, var.private_subnet_ids)
+  cluster_role_arn   = var.cluster_role_arn
+  node_role_arn      = var.node_role_arn
+
+  node_groups = {
+    eks_node_group = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+      key_name      = var.key_name
+    }
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_policy_attachment,
-    aws_iam_role_policy_attachment.eks_vpc_policy_attachment
-  ]
 }
-
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = var.eks_worker_role_arn
-  subnet_ids      = var.private_subnet_ids
-  instance_types  = [var.node_instance_type]
-  desired_size    = var.node_group_size
-  min_size        = var.node_group_size
-  max_size        = var.node_group_size
-
-  depends_on = [
-    aws_eks_cluster.main
-  ]
-}
-
