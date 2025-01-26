2512e208910c35e1d03a2ec52e9981c5a3a03a5b
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-26 17:53:46 +0530

    Update Output.tf

--- Module/Output.tf
+++ Module/Output.tf
@@ -10,10 +10,16 @@ output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
 
-output "eks_role_arn" {
-  value = aws_iam_role.eks_role.arn
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
 }
 
-output "node_role_arn" {
-  value = aws_iam_role.node_role.arn
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
+
+output "eks_cluster_id" {
+  value = module.eks.cluster_id
+}
+
+
