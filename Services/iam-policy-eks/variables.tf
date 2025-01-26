3ac412c5c29ff0703896bd393cb7db706bd7c53c
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-26 18:25:54 +0530

    Update variables.tf

--- Services/iam-policy-eks/variables.tf
+++ Services/iam-policy-eks/variables.tf
@@ -1,9 +1,1 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
 
-variable "eks_role_arn" {
-  description = "ARN of the IAM role for the EKS cluster"
-  type        = string
-}
