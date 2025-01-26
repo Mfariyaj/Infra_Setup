a02da753b07cbbb64a1d0441420cf0b6b8e741ba
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-26 20:04:03 +0530

    Update main.tf

--- Module/main.tf
+++ Module/main.tf
@@ -52,11 +52,11 @@ module "IAM_EKS_Role" {
 
 
 module "eks" {
-  source                = "../Services/EKS"
-  cluster_name          = var.eks_cluster_name
-  vpc_id                = module.vpc.vpc_id
-  eks_node_instance_type = var.eks_node_instance_type
-  node_group_size       = var.node_group_desired_size
-  eks_role_arn          = module.iam.eks_role_arn
+  source             = "../Services/EKS"
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+  cluster_role_arn   = module.iam.eks_cluster_role_arn
+  node_role_arn      = module.iam.eks_node_role_arn
 }
 
