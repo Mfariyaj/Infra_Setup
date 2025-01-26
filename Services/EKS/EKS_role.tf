7b0773d7673b755520a78ad9fe974febe9ed229f
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-26 16:25:25 +0530

    Update EKS_role.tf

--- Services/EKS/EKS_role.tf
+++ Services/EKS/EKS_role.tf
@@ -1,37 +1,33 @@
-# Creating IAM role for Master Node
-resource "aws_iam_role" "master" {
-  name = "EKS-Master"
+resource "aws_iam_role" "eks_role" {
+  name = "eks-role"
 
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "eks.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
     ]
   })
 }
 
-# Attaching Policy to IAM role
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.master.name
+  role       = aws_iam_role.eks_role.name
 }
 
-# Attaching Policy to IAM role
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.master.name
+  role       = aws_iam_role.eks_role.name
 }
 
-# Attaching Policy to IAM role
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.master.name
+  role       = aws_iam_role.eks_role.name
 }
 
 # Creating IAM role for Worker Node
@@ -52,7 +48,6 @@ resource "aws_iam_role" "worker" {
   })
 }
 
-# Creating IAM Policy for auto-scaler
 resource "aws_iam_policy" "autoscaler" {
   name = "ed-eks-autoscaler-policy"
   policy = jsonencode({
@@ -75,43 +70,27 @@ resource "aws_iam_policy" "autoscaler" {
   })
 }
 
-# Attaching Policy to IAM role
+# Attaching policies to Worker Role
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.worker.name
 }
 
-# Attaching Policy to IAM role
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.worker.name
 }
 
-# Attaching Policy to IAM role
 resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.worker.name
 }
 
-# Attaching Policy to IAM role
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.worker.name
 }
 
-# Attaching Policy to IAM role
-resource "aws_iam_role_policy_attachment" "x-ray" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
-  role       = aws_iam_role.worker.name
-}
-
-# Attaching Policy to IAM role
-resource "aws_iam_role_policy_attachment" "s3" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-  role       = aws_iam_role.worker.name
-}
-
-# Attaching Policy to IAM role
 resource "aws_iam_role_policy_attachment" "autoscaler" {
   policy_arn = aws_iam_policy.autoscaler.arn
   role       = aws_iam_role.worker.name
