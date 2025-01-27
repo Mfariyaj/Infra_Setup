b7117b1ec655b0b3bf1e1e8f91ea5bf7149f4b5b
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 14:43:13 +0530

    Update main.tf

--- Services/EKS/main.tf
+++ Services/EKS/main.tf
@@ -17,7 +17,7 @@ resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_role_arn   = aws_iam_role.eks_worker_role.arn
   node_group_name = "eks-node-group"
-  subnet_ids      = var.subnet_ids
+  subnet_ids      = var.public_subnets
   scaling_config {
     desired_size = 2
     max_size     = 3
