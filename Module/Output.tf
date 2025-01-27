47f1358b06e362e1247cfff098c99ac7e6d0f942
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 14:55:11 +0530

    Update Output.tf

--- Module/Output.tf
+++ Module/Output.tf
@@ -14,10 +14,6 @@ output "private_subnet_ids" {
 #  value = module.eks.cluster_name
 #}
 
-output "eks_cluster_id" {
-  value = module.eks.cluster_id
-}
-
 output "eks_cluster_endpoint" {
   value = module.eks.eks_cluster_endpoint
 }
