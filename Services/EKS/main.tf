c19ae70174c9dce4ee05385d4b5374f9db8c470b
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 20:23:08 +0530

    Update main.tf

--- Services/EKS/main.tf
+++ Services/EKS/main.tf
@@ -3,7 +3,8 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
   access_config {
-    authentication_mode = "API"
+    authentication_mode                         = "CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   vpc_config {
