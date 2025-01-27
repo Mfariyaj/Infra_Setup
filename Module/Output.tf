59a33c042ee05061419ad8fb793c28c945ec08d1
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 14:20:51 +0530

    Update Output.tf

--- Module/Output.tf
+++ Module/Output.tf
@@ -14,13 +14,18 @@ output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
 output "eks_cluster_id" {
   value = module.eks.cluster_id
 }
+
+output "eks_cluster_endpoint" {
+  value = module.eks.eks_cluster_endpoint
+}
+
+output "eks_cluster_arn" {
+  value = module.eks.eks_cluster_arn
+}
+
 
 
 
