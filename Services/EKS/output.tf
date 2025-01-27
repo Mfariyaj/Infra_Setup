6e6406f7492b36f78b2bbdd3cbbf80bed995eadf
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 14:49:58 +0530

    Update output.tf

--- Services/EKS/output.tf
+++ Services/EKS/output.tf
@@ -2,6 +2,11 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.eks_cluster.name
 }
 
+output "cluster_id" {
+  description = "ID of the EKS cluster"
+  value       = aws_eks_cluster.this.id
+}
+
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
