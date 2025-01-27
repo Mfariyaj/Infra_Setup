007219bf6187e6a325d58b936342315b052cec85
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 14:45:48 +0530

    Update output.tf

--- Services/EKS/output.tf
+++ Services/EKS/output.tf
@@ -7,13 +7,13 @@ output "eks_cluster_endpoint" {
 }
 
 output "eks_cluster_arn" {
-  value = aws_eks_cluster.eks_cluster.arn
+  value       = aws_iam_role.eks_cluster_role.arn
 }
 
 output "eks_worker_arn" {
-  value = aws_eks_cluster.eks_worker.arn
+  description = "ARN of the IAM role assigned to EKS worker nodes, allowing them to interact with AWS resources"
+  value       = aws_iam_role.eks_worker_role.arn
 }
-
 output "node_group_name" {
   value = aws_eks_node_group.eks_nodes.node_group_name
 }
