7a66ab3471ca21c5b87e9ea95dde52b36c1b6b6d
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 17:41:45 +0530

    Update main.tf

--- Services/EKS/main.tf
+++ Services/EKS/main.tf
@@ -2,7 +2,9 @@
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
-  type          = "STANDARD"
+  access_config {
+    authentication_mode = "API"
+  }
 
   vpc_config {
     subnet_ids = var.public_subnets
