c1ca1777238ed7542bda0d79761d7e3111fe994d
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-27 14:30:23 +0530

    Update main.tf

--- Module/main.tf
+++ Module/main.tf
@@ -51,7 +51,7 @@ module "IAM_EKS_Role" {
 }
 
 module "eks" {
-  source              = "./modules/eks"
+  source              = "../Services/EKS"
   cluster_name        = var.cluster_name
   vpc_id              = module.vpc.vpc_id
   public_subnets      = [module.vpc.public_subnet_ids]
