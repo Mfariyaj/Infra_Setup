4129be378f4f8fc4cfec01d35a0ba4ff593e220f
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-01-28 17:15:28 +0530

    Update EKS_role.tf

--- Services/EKS/EKS_role.tf
+++ Services/EKS/EKS_role.tf
@@ -72,3 +72,8 @@ resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
   
 }
 
+resource "aws_iam_role_policy_attachment" "SSMManagedInstanceCore" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  
+}
