d32b318a1a508438719e347447f6fe21ae7368a7
Shaikh Mohammed Fariyaj Najam <96516343+Mfariyaj@users.noreply.github.com>
2025-02-17 17:24:07 +0530

    Update main.tf

--- Services/EKS/main.tf
+++ Services/EKS/main.tf
@@ -32,20 +32,3 @@ resource "aws_eks_node_group" "eks_nodes" {
     Name = "EKS-Node-Group"
   }
 }
-
-# EKS Add-ons
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.eks_cluster.name
-  addon_name   = "kube-proxy"
-}
-
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name = aws_eks_cluster.eks_cluster.name
-  addon_name   = "aws-ebs-csi-driver"
-}
-
-resource "aws_eks_addon" "core_dns" {
-  cluster_name = aws_eks_cluster.eks_cluster.name
-  addon_name   = "coredns"
-}
-
